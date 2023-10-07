2053473d9064f2dfc5ba0d669b23cc48510c61b5
Abdul Basit <139325106+Abdul-Basit77@users.noreply.github.com>
2023-10-08 02:56:16 +0530

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -1,17 +1,22 @@
 from ultralytics import YOLO
-from threading import Thread
 import datetime
 import cvzone
 import cv2
 import math
+import os
 from Smail import send_email
 
 model = YOLO("faceModel.pt")
 classnames = ["face"]
 
+email_sent = False
 def face(frame):
+    output_folder = "Captured"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     result = model(frame,stream=True)
+    global email_sent
     
     for info in result:
         boxes = info.boxes
@@ -24,12 +29,23 @@ def face(frame):
                 x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
                 cvzone.putTextRect(frame, f'{classnames[Class]} {confidence}%', [x1-8,y1-10], scale=1.5, thickness=2)
+                if confidence > 80 and not email_sent:
+                timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                output_path = os.path.join(output_folder, f"face_{timestamp}.jpg")
+                cv2.imwrite(output_path, frame)
+                email_sent = True
+                message = "Someone was detected in restricted area"
+                send_email(message,output_path) 
+
     return frame
 
+email_sent2 = False
 def faceCounter(frame):
+    output_folder = "Captured"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     result = model(frame, stream=True)
-    EMstatus = False
-    thread2 = Thread(target=send_email)
+    global email_sent2
     faceDetected = 0
 
     current_time = datetime.datetime.now()
@@ -43,8 +59,16 @@ def faceCounter(frame):
         for box in boxes:
             confidence = box.conf[0]
             confidence = math.ceil(confidence * 100)
-            if confidence > 10:
+            if confidence > 40:
                 faceDetected += 1
-            if faceDetected > 4 and EMstatus == False and restricted_time==True:
-                thread2.start()
-                EMstatus = True
+                text = f"No. of Peoples: {faceDetected}"
+                position = (frame.shape[1] - 300, 50)
+                cv2.putText(frame, text, position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            if faceDetected > 4 and email_sent2 == False and restricted_time==True:
+                output_path = os.path.join(output_folder, f"face_{current_time}.jpg")
+                cv2.imwrite(output_path, frame)
+                email_sent2 = True
+                message = "Security Breach"
+                send_email(message,output_path)
+
+    return frame
